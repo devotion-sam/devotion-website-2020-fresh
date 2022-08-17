d943f528e9dc2dc7ecf65e32407d8fef7e9cfc3d
devotion-sam <sam@devotion.com.au>
2022-08-17 21:56:33 +1000

    u

--- islands/WorkCarousel.tsx
+++ islands/WorkCarousel.tsx
@@ -23,14 +23,16 @@ export default function WorkCarousel(
   }, []);
   return (
     <div class={tw`py-16 px-5`}>
-      <div className="work">
+      <div class={tw`relative`}>
         <div class={tw`swiper-container overflow-hidden`}>
           <div class="swiper-wrapper">
             {props.works.items.map((item) => {
               return (
                 <div class={tw`swiper-slide !w-96`}>
                   <img
-                    src={`${item.thumbnail}?w={width}&auto=format`}
+                    width={384}
+                    height={264}
+                    data-src={`${item.thumbnail}?w={width}&auto=format`}
                     sizes="auto"
                     class={tw`w-full lazyload`}
                   />
--- routes/index.tsx
+++ routes/index.tsx
@@ -72,6 +72,14 @@ export default function Home({ data }) {
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"
       />
+      <script
+        defer
+        src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js"
+        integrity="sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ=="
+        crossorigin="anonymous"
+        referrerpolicy="no-referrer"
+      >
+      </script>
       <Header />
       <HomepageHero />
       <CustomerExperienceBar
